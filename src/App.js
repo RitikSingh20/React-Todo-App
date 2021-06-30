c01be26c5f1256d2dfe4554eb9b59159befdbc4c
ritik723 <ritiksingh7239@gmail.com>
2021-06-30 13:40:10 +0530

    created add items in toods

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+ 
 import './App.css';
-import TodoForm from './components/TodoForm';
+ 
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-    <TodoForm/>
+    <TodoList/>
     </div>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,22 @@
 import React,{useState} from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
     const [input,setInput] = useState('');
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    };
 
     const handleSubmit = e => {
         e.preventDefault();
-        
+
+        props.onSubmit({
+            id:Math.floor(Math.random() * 10000),
+            text:input
+        });
+        setInput('');
     };
+    
     return (
         <form className='todo-form'>
             <input
@@ -14,9 +24,10 @@ function TodoForm() {
             plaacholder='Add a photo'
             value={input}
             nmae='text'
-            className='todo-button'
+            className='todo-input'
+            onChange={handleChange}
             />
-            <button className="todo-button">Add todo</button>
+            <button className="todo-button" onClick={handleSubmit}>Add todo</button>
         </form>
     )
 }
